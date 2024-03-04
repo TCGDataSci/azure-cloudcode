085df5b99431b851db970b0cde6f314c1a118f24
mbocker <mb@tremcap.com>
2024-03-03 22:10:34 -0500

    removed useless imports

--- functionapps/linux-python-starter/function_app.py
+++ functionapps/linux-python-starter/function_app.py
@@ -1,11 +1,5 @@
 # azure imports 
 import azure.functions as func
-import azure.durable_functions as df  
-from azure.identity import EnvironmentCredential
-from azure.keyvault.secrets import SecretClient
-
-# tcgds imports
-import tcgds
 
 # other imports
 import requests
