75f6887f12fd06bb710cc6d770974a275b9d0699
mbocker <mb@tremcap.com>
2024-03-03 22:09:38 -0500

    updated starter functionapp code

--- functionapps/linux-python-starter/function_app.py
+++ functionapps/linux-python-starter/function_app.py
@@ -13,7 +13,11 @@ import requests
 
 app = func.FunctionApp()
 
+
+
 ### APIS ###
+
+# sensortower update  
 @app.timer_trigger('timer', '0 0 11 * * *', run_on_startup=False)
 def sensortower_update(timer:func.TimerRequest):
     if timer.past_due:
@@ -21,7 +25,7 @@ def sensortower_update(timer:func.TimerRequest):
     response = requests.get("https://linux-python-apimanager.azurewebsites.net/api/apis/sensortower/update")
     return response
 
-
+# similarweb update
 @app.timer_trigger('timer', '0 0 11 * * *', run_on_startup=False)
 def similarweb_update(timer:func.TimerRequest):
     if timer.past_due:
@@ -29,7 +33,7 @@ def similarweb_update(timer:func.TimerRequest):
     response = requests.get("https://linux-python-apimanager.azurewebsites.net/api/apis/similarweb/update")
     return response
 
-
+# whalewisdom update
 @app.timer_triger('timer', '0 0 12,14,16,18,20 13-21 2,5,8,11 *' , run_on_startup=False)
 def whalewisdom_update(timer:func.TimerRequest):
     if timer.past_due:
@@ -37,7 +41,11 @@ def whalewisdom_update(timer:func.TimerRequest):
     response = requests.get("https://linux-python-apimanager.azurewebsites.net/api/apis/whalewisdom/13fupdate")
     return response
 
+
+
 ### SMALL SCRAPES ###
+
+# dutchbros location scrape
 @app.timer_trigger('timer', "0 0 17 * * 0", run_on_startup=False)
 def dutchbros_location_scrape(timer:func.TimerRequest):
     if timer.past_due:
@@ -45,7 +53,7 @@ def dutchbros_location_scrape(timer:func.TimerRequest):
     response = requests.get("https://linux-python-small-scrapes.azurewebsites.net/api/scrapes/bros/locations")
     return response
 
-
+# avdx sales person schedule scrape
 @app.timer_trigger('timer', "0 0 8 * * 1-5", run_on_startup=False)
 def avdx_sales_schedule_scrape(timer:func.TimerRequest):
     if timer.past_due:
@@ -56,27 +64,29 @@ def avdx_sales_schedule_scrape(timer:func.TimerRequest):
 
 
 ### LARGE SCRAPES ###
-# @app.timer_trigger('timer', '', run_on_startup=False)
-# def bookingdotcom_ushotels_scrape(timer:func.TimerRequest):
-#     if timer.past_due:
-#         pass
-#     response = requests.get("https://linux-python-bookingdotcom.azurewebsites.net/api/scrapes/bookingdotcom/ushotels")
-#     return response
 
+# booking.com scrape
+@app.timer_trigger('timer', '0 0 8 1 * *', run_on_startup=False)
+def bookingdotcom_ushotels_scrape(timer:func.TimerRequest):
+    if timer.past_due:
+        pass
+    response = requests.get("https://linux-python-bookingdotcom.azurewebsites.net/api/scrapes/bookingdotcom/ushotels")
+    return response
 
-# @app.timer_trigger('timer', '', run_on_startup=False)
-# def chtr_address_scrape(timer:func.TimerRequest):
-#     if timer.past_due:
-#         pass
-#     response = requests.get("https://linux-python-chtr.azurewebsites.net/api/scrapes/chtr/rando_addresses")
-#     return response
+# charter random address scrape
+@app.timer_trigger('timer', '0 0 9 1 * *', run_on_startup=False)
+def chtr_address_scrape(timer:func.TimerRequest):
+    if timer.past_due:
+        pass
+    response = requests.get("https://linux-python-chtr.azurewebsites.net/api/scrapes/chtr/rando_addresses")
+    return response
 
-
-# @app.timer_trigger('timer', '', run_on_startup=False)
-# def chtr_zipcode_scrape(timer:func.TimerRequest):
-#     if timer.past_due:
-#         pass
-#     response = requests.get("https://linux-python-chtr.azurewebsites.net/api/scrapes/chtr/zip_codes")
-#     return response
+# charter zip code scrape
+@app.timer_trigger('timer', '0 0 9 15 * *', run_on_startup=False)
+def chtr_zipcode_scrape(timer:func.TimerRequest):
+    if timer.past_due:
+        pass
+    response = requests.get("https://linux-python-chtr.azurewebsites.net/api/scrapes/chtr/zip_codes")
+    return response
 
 
