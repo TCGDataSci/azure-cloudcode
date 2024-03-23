7bb9291fbf8bd8922b87e9a05b2048ed12335cac
mbocker <mb@tremcap.com>
2024-03-22 21:56:39 -0400

    ffs

--- functionapps/linux-python-dks/function_app.py
+++ functionapps/linux-python-dks/function_app.py
@@ -5,4 +5,49 @@ from azure.identity import EnvironmentCredential
 from azure.keyvault.secrets import SecretClient
 
 # tcgds imports
-import tcgds
+from tcgds.scrapes.dks import *
+from tcgds.postgres import PandasPGHelper
+
+# other imports 
+import uuid
+from datetime import datetime
+
+KVUrl = "https://tcgdsvault.vault.azure.net/"
+credential = EnvironmentCredential()
+secret_client = SecretClient(vault_url=KVUrl, credential=credential)
+  
+# psql constants     
+psql_username = secret_client.get_secret('PSQLUsername').value  
+psql_password = secret_client.get_secret('PSQLPassword').value
+storage_connection_string = secret_client.get_secret('maintcgdssaConnectionString')
+
+app = func.FunctionApp()
+
+
+@app.route("scrapes/dks/products")
+def dks_product_scrape(req:func.HttpRequest):
+    scrape_guid = uuid.uuid4()
+    location_df = get_dks_locations_sitemap()
+    product_search_results = get_all_products_search('on')
+    skus_meta_df, skus_attributes_df = get_skus_data(product_search_results['dsg_seo_url'].to_list(), return_attributes=True, report=True)
+    skus_inventory_df = get_inventory_data(skus_meta_df['sku_id'].to_list(), location_df['store_code'].astype(str).to_list())
+    skus_inventory_df = skus_inventory_df.merge(skus_meta_df[['sku_id', 'product_id']], how='left', left_on='sku', right_on='sku_id').drop('sku', axis=1)
+    pg_helper = PandasPGHelper(user=psql_username, password=psql_password)
+    date_today = datetime.today()
+    location_df['observed_date'] = [date_today]*len(location_df)
+    product_search_results['observed_date'] = [date_today]*len(product_search_results)
+    skus_meta_df['observed_date'] = [date_today]*len(skus_meta_df)
+    skus_inventory_df['observed_date'] = [date_today]*len(skus_inventory_df)
+    skus_attributes_df['observed_date'] = [date_today]*len(skus_attributes_df)
+    
+    location_df['scrape_guid'] = [scrape_guid]*len(location_df)
+    product_search_results['scrape_guid'] = [scrape_guid]*len(product_search_results)
+    skus_meta_df['scrape_guid'] = [scrape_guid]*len(skus_meta_df)
+    skus_inventory_df['scrape_guid'] = [scrape_guid]*len(skus_inventory_df)
+    skus_attributes_df['scrape_guid'] = [scrape_guid]*len(skus_attributes_df)
+    
+    pg_helper.to_sql(location_df, 'store_locations', 'dks')
+    pg_helper.to_sql(product_search_results, 'product_data', 'dks')
+    pg_helper.to_sql(skus_meta_df, 'skus_meta_data', 'dks')
+    pg_helper.to_sql(skus_attributes_df, 'skus_attributes_data', 'dks')
+    pg_helper.to_sql(skus_inventory_df, 'skus_inventory_data', 'dks')
--- functionapps/linux-python-small-scrapes/function_app.py
+++ functionapps/linux-python-small-scrapes/function_app.py
@@ -6,7 +6,6 @@ from azure.storage.blob import BlobServiceClient
 
 # tcgds import
 from tcgds.scrapes.bros import location_scrape as bros_location_scrape
-from tcgds.scrapes.five import JsonLocationUrls 
 from tcgds.reporting import report, send_report
 from tcgds.scrape import base_headers, SCRAPE_DATA_CONTAINER, SCRAPE_ERROR_CONTAINER, PARSE_ERROR_CONTAINER, custom_format_response
 from tcgds.postgres import psql_connection, Postgres, PandasPGHelper
@@ -74,75 +73,25 @@ def bros_location_scrape(req:func.HttpRequest):
 
 
 ### five below loaction scrapes ###
+from tcgds.scrapes.five import extract_location_data, location_url_generator
+
 @app.route("scrapes/five/locations")
 def five_location_scrape(req:func.HttpRequest):
-    # scrape variables
-    scrape_guid = uuid.uuid4().hex
-    scrape_name = "location_scrape" # for saving responses
-    domain_name = "fivebelow.com" # for saving responses
-    blob_path_prefix = f'{domain_name}/{scrape_name}/{scrape_guid}/' # for saving blob files
-
-    # postgres variables
-    pg_table = "" # for parsing
-    pg_schema = "bookingdotcom" # for parsing
-    pdpg_helper = PandasPGHelper(user=psql_username, password=psql_password)
-
-    # initialize requests session
-    throttle = 8
-    total_retries = 2 
-    backoff_factor = 2
-    status_forcelist = [400, 401, 404, 500]
-    s = requests.Session()
-    r = requests.adapters.Retry(total=total_retries, backoff_factor=backoff_factor, status_forcelist=status_forcelist)
-    s.mount('https://', requests.adapters.HTTPAdapter(max_retries=r)) 
-    s.headers = base_headers
-
-    # scrape 1
-    def url_generator(state:list):
-        if len(state)==0:
-            location_sitemap_url = 'https://locations.fivebelow.com/sitemap.xml'
-            response = requests.get(location_sitemap_url, headers=base_headers)
-            soup = BeautifulSoup(response.text, features='lxml') 
-            locations = [elt.text for elt in soup.find_all('loc') if len(elt.text.split('/'))>5]
-            state = locations
-        while len(state)>0:
-            yield state.pop(0)
-
-
-    # scrape
-    state = dict()
-    for url in url_generator(state):
+    import time 
+    extracted_data = []
+    throttle = 3
+    for url in location_url_generator(): 
+        headers = base_headers.copy()
+        headers['Referer'] = 'https://locations.fivebelow.com/coming-soon.html'
+        headers['Accept'] = 'application/json'
+        response = requests.get(url, headers=headers)
+        json_obj = response.json()
+        entities = json_obj['response']['entities']
+        extracted_data.extend([extract_location_data(entity) for entity in entities])
         time.sleep(throttle)
-        try:
-            response = s.get(url)
-            custom_response = custom_format_response(response, scrape_guid=scrape_guid)
-            blob_name = blob_path_prefix+custom_response.url_uuid+'-'+datetime.strptime(custom_response['datetime'], "%Y-%m-%d %H:%M:%S").timestamp()+'.txt'
-            with BlobServiceClient.from_connection_string(storage_connection_string) as blob_service_client:
-                if (status_code:=response.status_code)>=200 and status_code<300:
-                    # mutli thread these sections
-                    blob_client = blob_service_client.get_blob_client(SCRAPE_DATA_CONTAINER, blob_name)
-                    blob_client.upload_blob(json.dumps(custom_response))
-
-                    # parse
-                    try:
-                        response_obj = custom_response['response']
-                        soup = BeautifulSoup(response_obj)
-                        extracted_data = parser(soup)
-                        pdpg_helper.to_sql(extracted_data, pg_table, pg_schema)
-                    except Exception as e:
-                        blob_client = blob_service_client.get_blob_client(PARSE_ERROR_CONTAINER, blob_name)
-                        blob_client.upload_blob(json.dumps(custom_response))
-
-                elif status_code>=400 and status_code<500:
-                    blob_client = blob_service_client.get_blob_client(SCRAPE_ERROR_CONTAINER, blob_name)
-                    blob_client.upload_blob(json.dumps(custom_response))
-                    pass
-        except Exception as e:
-            pass
-    return 'success'
-
-
-
+    data_df = pd.DataFrame(extracted_data)
+    pg_helper = PandasPGHelper(user=psql_username, password=psql_password)
+    pg_helper.to_sql(data_df, table_name='store_locations_1', schema='five_stuff', if_exists='replace')    
 
 
 ### avdx sales schedule scrape ### 
