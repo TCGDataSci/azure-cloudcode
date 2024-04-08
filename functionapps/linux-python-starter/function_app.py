295d99856dfc95e1a87e61b4690661f2e432d974
mbocker <mb@tremcap.com>
2024-04-08 09:55:03 -0400

    running bookingdotcom and chtr zipcode scrape

--- functionapps/linux-python-starter/function_app.py
+++ functionapps/linux-python-starter/function_app.py
@@ -98,7 +98,7 @@ def chtr_address_scrape(timer:func.timer.TimerRequest):
     return response
 
 # charter zip code scrape
-@app.timer_trigger('timer', '0 0 9 15 * *', run_on_startup=False)
+@app.timer_trigger('timer', '0 0 9 15 * *', run_on_startup=True)
 def chtr_zipcode_scrape(timer:func.timer.TimerRequest):
     if timer.past_due:
         pass
